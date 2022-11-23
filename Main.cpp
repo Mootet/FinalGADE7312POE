1671ea0988f7faff3ff282bfddd980d707370411
Mootet <cw924094@gmail.com>
2022-11-23 15:07:23 +0200

    Added shaders

--- Main.cpp
+++ Main.cpp
@@ -12,17 +12,17 @@
 
 GLfloat vertices[] =
 {
-	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 
+	-0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,	 0.8f, 0.3f,  0.02f,
 
-	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f, 
+	0.5f, -0.5f * float(sqrt(3)) / 3, 0.0f,		 0.8f, 0.3f,  0.02f,
 
-	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f, 
+	0.0f, 0.5f * float(sqrt(3)) * 2 / 3, 0.0f,		1.0f, 0.6f,  0.32f,
 
-	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, 
+	-0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,		 0.9f, 0.45f, 0.17f,
 
-	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f, 
+	0.5f / 2, 0.5f * float(sqrt(3)) / 6, 0.0f,		0.9f, 0.45f, 0.17f,
 
-	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f 
+	0.0f, -0.5f * float(sqrt(3)) / 3, 0.0f,			0.8f, 0.3f,  0.02f
 };
 
 GLuint indices[] =
@@ -71,12 +71,13 @@ int main()
 	VBO VBO1(vertices, sizeof(vertices));
 	EBO EBO1(indices, sizeof(indices));
 
-	VAO1.LinkVBO(VBO1, 0);
+	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 6 * sizeof(float), (void*)0);
+	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 6 * sizeof(float), (void*)(3 * sizeof(float)));
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-
+	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -86,6 +87,7 @@ int main()
 
 
 		shaderProgram.Activate();
+		glUniform1f(uniID, 0.5f);
 
 		VAO1.Bind();
 
