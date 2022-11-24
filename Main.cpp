4e99d912edc1bcc42a81ff5ed917b02b253c4f7a
Mootet <cw924094@gmail.com>
2022-11-24 03:03:39 +0200

    Camera

--- Main.cpp
+++ Main.cpp
@@ -11,6 +11,7 @@
 #include"VAO.h"
 #include"VBO.h"
 #include"EBO.h"
+#include"Camera.h"
 
 
 const unsigned int width = 800;
@@ -42,7 +43,6 @@ GLuint indices[] =
 
 int main()
 {
-	// Initialize GLFW
 	glfwInit();
 
 	
@@ -60,83 +60,50 @@ int main()
 		glfwTerminate();
 		return -1;
 	}
-
 	glfwMakeContextCurrent(window);
-
 
 	gladLoadGL();
 	
 	glViewport(0, 0, width, height);
 
 
-
 	Shader shaderProgram("default.vert", "default.frag");
-
 
 
 	VAO VAO1;
 	VAO1.Bind();
 
 	VBO VBO1(vertices, sizeof(vertices));
-
 	EBO EBO1(indices, sizeof(indices));
 
 	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
 	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
 	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-
 	VAO1.Unbind();
 	VBO1.Unbind();
 	EBO1.Unbind();
 
-	
-	GLuint uniID = glGetUniformLocation(shaderProgram.ID, "scale");
+
 
 	
-	
-
-	
-	Texture brickTex("Chessnut.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
+	Texture brickTex("brick.png", GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
 	brickTex.texUnit(shaderProgram, "tex0", 0);
 
-	float rotation = 1.0f;
-	double prevTime = glfwGetTime();
+
 
 	glEnable(GL_DEPTH_TEST);
 
-	
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
+
 	while (!glfwWindowShouldClose(window))
 	{
-		
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
 		shaderProgram.Activate();
 
-		double crntTime = glfwGetTime();
-		if (crntTime - prevTime >= 1.0f / 60.0f)
-		{
-			rotation += -5.5f;
-			prevTime = crntTime;
-		}
+		camera.Inputs(window);
+		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
 
-		glm::mat4 model = glm::mat4(1.0f);
-		glm::mat4 view = glm::mat4(1.0f);
-		glm::mat4 proj = glm::mat4(1.0f);
-
-		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, -0.5f, -2.0f));
-		proj = glm::perspective(glm::radians(45.0f), (float)width / height, 0.1f, 100.0f);
-
-		int modelLoc = glGetUniformLocation(shaderProgram.ID, "model");
-		glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-		int viewLoc = glGetUniformLocation(shaderProgram.ID, "view");
-		glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-		int projLoc = glGetUniformLocation(shaderProgram.ID, "proj");
-		glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(proj));
-
-		glUniform1f(uniID, 0.5f);
 		brickTex.Bind();
 		VAO1.Bind();
 		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
